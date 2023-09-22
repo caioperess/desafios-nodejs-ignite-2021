f2e9ff665de8665b326e5819709c982bff653fe7
Caio Peres <caioperescs@hotmail.com>
2023-09-22 14:50:47 -0300

    Desafio 04 update

--- desafio04-nodejs-introducao-ao-SOLID/src/server.ts
+++ desafio04-nodejs-introducao-ao-SOLID/src/server.ts
@@ -1,3 +1,11 @@
+import path from "path";
+import swaggerUi from "swagger-ui-express";
+import YAML from "yamljs";
+
 import { app } from ".";
+
+const swaggerFile = YAML.load(path.resolve(__dirname, "./swagger.yaml"));
+
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
 
 app.listen(3333, () => console.log("Server is running!"));
